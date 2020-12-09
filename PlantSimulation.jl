0ad6d7cd6b2f5b60f1586ef2efcfe3fd1d14c226
Regan Baucke <Regan.Baucke@airnz.co.nz>
2020-12-10 09:27:07 +1300

    added cleaning status

--- PlantSimulation.jl
+++ PlantSimulation.jl
@@ -5,9 +5,10 @@
 module PlantSimulation
 
 # basic structs
-# a pasturiser has a operating status and time in that state
+# a pasturiser has a operating mode and time in that state
 struct Pasturiser
-   status
+   mode
+   clean_status
    time
 end
 
@@ -19,22 +20,22 @@ end
 
 # constructors for pasturisers
 function boot_up(pasturiser)
-   Pasturiser("BOOTING UP",0.0)
+   Pasturiser("BOOTING UP","DIRTY",0.0)
 end
 function turn_on(pasturiser)
-   Pasturiser("ON",0.0)
+   Pasturiser("ON","DIRTY",0.0)
 end
 function turn_off(pasturiser)
-   Pasturiser("OFF",0.0)
+   Pasturiser("OFF","DIRTY",0.0)
 end
 function clean(pasturiser)
-   Pasturiser("CLEANING",0.0)
+   Pasturiser("CLEANING","DIRTY",0.0)
 end
 
 # for a given bank of pasturisers, calculate the production_rate
 function production_rate(bank)
    map(bank) do pas
-      if (pas.status == "ON") || (pas.status == "BOOTING UP")
+      if (pas.mode == "ON") || (pas.mode == "BOOTING UP")
          55.0
       else
          0.0
@@ -54,7 +55,7 @@ function turn_on_first_available(bank)
    turned_on = false
    result = Pasturiser[]
    for pas in bank
-      if (pas.status != "OFF") || turned_on
+      if (pas.mode != "OFF") || turned_on || (pas.clean_status == "DIRTY")
          push!(result, pas)
       else
          push!(result,boot_up(pas))
@@ -64,25 +65,39 @@ function turn_on_first_available(bank)
    result
 end
 
+# create a new bank which is the same as the old bank but with the first dirty and off pasturiser being cleaned
+function clean_first_available(bank)
+   cleaned = false
+   result = Pasturiser[]
+   for pas in bank
+      if (!cleaned) && (pas.mode == "OFF") && (pas.clean_status == "DIRTY")
+         push!(result,Pasturiser("CLEANING","DIRTY",0.0))
+      else
+         push!(result,pas)
+      end
+   end
+   result
+end
+
 # define the forced transition functions, if a pasturiser has been in a certain state 
 # for a given amount of time, make the transition to the appropriate state
 function transition_from_booting(pasturiser)
-   if pasturiser.status == "BOOTING UP" && pasturiser.time >= 60
-      Pasturiser("ON",0.0)
+   if pasturiser.mode == "BOOTING UP" && pasturiser.time >= 60
+      Pasturiser("ON","DIRTY",0.0)
    else
       pasturiser
    end
 end
 function transition_from_cleaning(pasturiser)
-   if pasturiser.status == "CLEANING" && pasturiser.time >= 180
-      Pasturiser("OFF",0.0)
+   if pasturiser.mode == "CLEANING" && pasturiser.time >= 180
+      Pasturiser("OFF","CLEAN",0.0)
    else
       pasturiser
    end
 end
 function transition_from_on(pasturiser)
-   if pasturiser.status == "ON" && pasturiser.time >= 420
-      Pasturiser("OFF",0.0)
+   if pasturiser.mode == "ON" && pasturiser.time >= 420
+      Pasturiser("OFF","DIRTY",0.0)
    else
       pasturiser
    end
@@ -91,9 +106,9 @@ transition = transition_from_booting ∘ transition_from_cleaning ∘ transition
 
 # Add some running time to pasturisers
 function add_duration(time,pasturiser)
-   Pasturiser(pasturiser.status,pasturiser.time + time)
+   Pasturiser(pasturiser.mode,pasturiser.clean_status,pasturiser.time + time)
 end
 
-export Pasturiser, Plant, transition, boot_up, turn_on, clean, production_rate, silo_level, turn_on_first_available, add_duration
+export Pasturiser, Plant, transition, boot_up, turn_on, clean, production_rate, silo_level, turn_on_first_available, clean_first_available, add_duration
 
 end
--- main.jl
+++ main.jl
@@ -5,23 +5,35 @@ using .DiscreteSystem
 using .PlantSimulation
 
 
-# describe initial state
+# describe initial state, 4 clean and off pasturisers and a silo level of 200
 function my_initial_state()
-   Plant(200,[Pasturiser("OFF",0.0) for i in 1:4])
+   Plant(200,[Pasturiser("OFF","CLEAN",0.0) for i in 1:4])
 end
 
 #
-# Define a generic policy (possibly bad) to govern the operations of the plant
-#
+# Define a generic policy (possibly bad) to govern the operations of the plant.
+# 
+# This policy has three parts:
+#     for all pasturisers, transition them out of their current state if imposed by the rules e.g. if a pasturiser has been in cleaning for 3 hours, move to off state
+#     after these pasturisers have made their forced transitions, turn on a pasturiser if the current production rate and silo level are sufficiently low
+#     after this change to the bank of pasturisers, add some "running time" to these pasturisers in their new state
+
 function my_factory_policy(plant)
    # do forced transitions
    transitioned_pas = map(transition,plant.bank)
 
-   # do chosen transitions
+   # turn on a pasturiser if neccesary
    acted_pas = if (production_rate(transitioned_pas) < 100) && (plant.silo < 200)
       turn_on_first_available(transitioned_pas)
    else
       transitioned_pas
+   end
+
+   # clean a pasturiser if there is one free
+   acted_pas = if (map(x->x.mode != "CLEANING",acted_pas) |> all) 
+      clean_first_available(acted_pas)
+   else
+      acted_pas
    end
 
    # add time to the plant
@@ -33,5 +45,11 @@ end
 
 
 # simulate the plant from its initial state using the above policy for 240 minutes
-my_simulation = build_discrete_system(my_factory_policy, my_initial_state(),0.0,240)
+my_simulation = build_discrete_system(my_factory_policy, my_initial_state(),0.0,700)
 
+
+# with this my simulation object (just an array of states and times) we would be able to ask certain questions about it
+# such as: is it feasible?
+#          feasibility_test(my_condition, my_simulation)
+# and: how many cleans did we do
+#          how_many_cleans(my_simulation)
