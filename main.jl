914bed5137b1dfacde1092aab91fc8a816dbd090
Regan Baucke <Regan.Baucke@airnz.co.nz>
2020-12-10 08:29:49 +1300

    general clean up

--- main.jl
+++ main.jl
@@ -5,10 +5,14 @@ using .DiscreteSystem
 using .PlantSimulation
 
 
+# describe initial state
 function my_initial_state()
    Plant(200,[Pasturiser("OFF",0.0) for i in 1:4])
 end
 
+#
+# Define a generic policy (possibly bad) to govern the operations of the plant
+#
 function my_factory_policy(plant)
    # do forced transitions
    transitioned_pas = map(transition,plant.bank)
@@ -24,7 +28,10 @@ function my_factory_policy(plant)
    final_pas = map(x -> add_duration(5.0,x),acted_pas)
 
    #return new plant state, with new silo level
-   (Plant(silo_level(plant.silo, final_pas), final_pas),5.0)
+   (Plant(silo_level(plant.silo, final_pas), final_pas), 5.0)
 end
 
-build_discrete_system(my_factory_policy, my_initial_state(),0.0,240)
+
+# simulate the plant from its initial state using the above policy for 240 minutes
+my_simulation = build_discrete_system(my_factory_policy, my_initial_state(),0.0,240)
+
